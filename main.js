75ab17647f00521c0fec2bf747bf67d43841a313
xmatid <70480433+xmatid@users.noreply.github.com>
2020-08-31 13:07:39 +0200

    usunięto nie działającą funkcje: odtwaranie muzyki

--- main.js
+++ main.js
@@ -24,40 +24,6 @@ client.once('ready', () => {
     client.user.setActivity('każdy Twój ruch', { type: "WATCHING"}).catch(console.error);
 });
 
-
-client.on('message', async message =>{
-   if(!message.content.startsWith(prefix) || message.author.bot)  return;  
-
-   const args = message.content.slice(prefix.length).split(/ +/);
-   const command = args.shift().toLowerCase(); 
-
-   if(message.content.startsWith(`${prefix}play`)){
-       const voiceChannel = message.member.voice.channel
-       if(!voiceChannel) return message.channel.send("Napierw muszisz być na kanale głosowym aby słuchać dobrych kawałków!")
-       const permissions = voiceChannel.permissionsFor(message.client.user)
-       if(!permissions.has('CONNECT')) return message.channel.send("Nie mam uprawnień aby połączyć się z tym kananłem.")
-       if(permissions.has('SPEAK')) return message.channel.send("Nie mam uprawnień aby grać na tym kanale.")
-       try {
-           var connection = await voiceChannel.join()
-        } catch (error) {
-            console.log(`Wystąpił błąd podczas dołączania do kanału głosowego: ${error}`)
-            return message.channel.send(`Wystąpił błąd podczas dołączania do kanału głosowego: ${error}`)
-        }
-
-        const dispatcher = connection.play(ytdl(args[1]))
-        .on('finish', () => {
-            voiceChannel.leave()
-        })
-        .on('error', error => {
-            console.log(error)
-        })
-        dispatcher.setVolumeLogarithmic(5 / 5)
-   } else if (message.content.startsWith(`${prefix}stop`)){
-       if(!message.member.voice.channel) return message.channel.send("Musisz być na kanale głosowym aby móc mnie zatrzymać.")
-       message.member.voice.leave
-       return undefined
-   }
-
    if(command === 'youtube'){
     client.commands.get('youtube').execute(message, args);
    }
@@ -73,6 +39,6 @@ client.on('message', async message =>{
         client.commands.get('komendy').execute(message, args);}
     else if (command == 'www'){
         client.commands.get('www').execute(message, args);}
-});
+
 
 client.login(process.env.token)
