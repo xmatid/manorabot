fd6542060d0b453560588ab4c22bda19460c9cac
xmatid <70480433+xmatid@users.noreply.github.com>
2020-09-01 23:30:24 +0200

    update: poprawki w tekście

--- komendy/dajmirole.js
+++ komendy/dajmirole.js
@@ -4,9 +4,9 @@ module.exports = {
     execute(message, args){
         
         if(message.member.roles.cache.has('709780848123314236')){
-            message.channel.send('__**Już masz role :D**__')
+            message.channel.send('***Już masz role :D***')
         } else {
-            message.channel.send('__**Widzę że nie masz roli, dobrze że się upominasz, masz swoją role :DD**__');}
+            message.channel.send('**Widzę że nie masz roli, dobrze że się upominasz, masz swoją role :DD**__');}
             message.member.roles.add('709780848123314236').catch(console.error);
         }
 }
--- komendy/komendy.js
+++ komendy/komendy.js
@@ -3,11 +3,11 @@ module.exports = {
     description: "Ta komenda pokazuje wszystkie komendy w bocie",
     execute(message, args){
         message.channel.send("```Wszyskie komendy jakie obsługuje bot (lista będzie aktualizowana wraz z botem):```");
-        message.channel.send("__**-dajmirole =**__ daje rolę Członek Serwera");
-        message.channel.send("__**-najnowszyfilm =**__ Pokazuje najnowszy film MaTiDa");
-        message.channel.send("__**-omatidzie =**__ NAJLEPSZA KOMENDA MUSISZ WPISAĆ");
-        message.channel.send("__**-youtube =**__ Daje link do kanału MaTiDa na YT");
-        message.channel.send("__**-www =**__ Daje link do strony internetowej serwera");
+        message.channel.send("**-dajmirole =** *daje rolę Członek Serwera*");
+        message.channel.send("**-najnowszyfilm =** *Pokazuje najnowszy film MaTiDa*");
+        message.channel.send("**-omatidzie =** *NAJLEPSZA KOMENDA MUSISZ WPISAĆ*");
+        message.channel.send("**-youtube =** *Daje link do kanału MaTiDa na YT*");
+        message.channel.send("**-www =** *Daje link do strony internetowej serwera*");
     }
 
 }
