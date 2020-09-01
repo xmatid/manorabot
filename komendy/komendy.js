f5c51f3c8ce15fb022f381aaa2ce67c249ee43fa
xmatid <70480433+xmatid@users.noreply.github.com>
2020-09-01 22:53:24 +0200

    update: dodano nowy format komend + bot wykrywa źle wpisaną komende

--- komendy/komendy.js
+++ komendy/komendy.js
@@ -2,6 +2,12 @@ module.exports = {
     name: 'komendy',
     description: "Ta komenda pokazuje wszystkie komendy w bocie",
     execute(message, args){
-        message.channel.send("Wszyskie komendy jakie obsługuje bot (lista będzie aktualizowana wraz z botem): -dajmirole = daje rolę Członek Serwera, -najnowszyfilm = Pokazuje najnowszy film MaTiDa, -omatidze = NAJLEPSZA KOMENDA MUSISZ WPISAĆ -youtube = Daje link do kanału MaTiDa na YT -www = Daje link do strony internetowej serwera");
+        message.channel.send("Wszyskie komendy jakie obsługuje bot (lista będzie aktualizowana wraz z botem): ");
+        message.channel.send("-dajmirole = daje rolę Członek Serwera");
+        message.channel.send("-najnowszyfilm = Pokazuje najnowszy film MaTiDa");
+        message.channel.send("-omatidzie = NAJLEPSZA KOMENDA MUSISZ WPISAĆ");
+        message.channel.send("-youtube = Daje link do kanału MaTiDa na YT");
+        message.channel.send("-www = Daje link do strony internetowej serwera");
     }
+
 }
--- main.js
+++ main.js
@@ -44,5 +44,35 @@ client.on('message', message =>{
     else if (command == 'www'){
         client.commands.get('www').execute(message, args);}
 });
-
+client.on("message", async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    const serverQueue = queue.get(message.guild.id);
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase(); 
+  
+    if (message.content.startsWith(`${prefix}youtube`)) {
+        youtube(message, serverQueue);
+        return;
+    } else if (message.content.startsWith(`${prefix}omatidzie`)) {
+      omatidzie(message, serverQueue);
+      return;
+    } else if (message.content.startsWith(`${prefix}dajmirole`)) {
+      dajmirole(message, serverQueue);
+      return;
+    } else if (message.content.startsWith(`${prefix}najnowszyfilm`)) {
+      najnowszyfilm(message, serverQueue);
+      return;
+    } else if (message.content.startsWith(`${prefix}komendy`)) {
+      komendy(message, serverQueue);
+      return;
+    } else if (message.content.startsWith(`${prefix}www`)) {
+      www(message, serverQueue);
+      return;
+    } else {
+      message.channel.send("Wpisz poprawną komendę! Jeżeli nie znasz komend, wpisz -komendy");
+    } 
+  
+  });
 client.login(process.env.token)
